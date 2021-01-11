15a973fd74ca7ff2e8a052969c3fce5d41653b29
sales <sales@orchardskills.com>
2021-01-11 14:12:26 -0700

    Add source

---
+++ OrchardSkills.OrchardCore.RaspberryPi/Controllers/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrchardSkills.OrchardCore.RaspberryPi.Devices;
+
+namespace OrchardSkills.OrchardCore.RaspberryPi.Controllers
+{
+
+    public class CameraController  : Controller
+    {
+        private readonly CameraDevice _cameraDevice;
+
+        public CameraController (CameraDevice cameraDevice)
+        {
+            _cameraDevice = cameraDevice;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CaptureImage()
+        {
+            _cameraDevice.CaptureImage();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ OrchardSkills.OrchardCore.RaspberryPi/Devices/CameraDevice.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using Unosquare.RaspberryIO;
+
+namespace OrchardSkills.OrchardCore.RaspberryPi.Devices
+{
+    public class CameraDevice : IDisposable
+    {
+        private bool disposedValue = false;
+
+        public void CaptureImage()
+        {
+            try
+            {
+                var pictureBytes = Pi.Camera.CaptureImageJpeg(640, 480);
+                var targetPath = "/home/pi/picture.jpg";
+                if (File.Exists(targetPath))
+                    File.Delete(targetPath);
+                File.WriteAllBytes(targetPath, pictureBytes);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+    }
+}
